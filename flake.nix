5e952a22db578383fb80f554e43ec717ea98dd3f
Kieran Klukas <me@dunkirk.sh>
2026-01-10 16:07:04 +0700

    feat: add package

--- flake.nix
+++ flake.nix
@@ -3,31 +3,71 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            go_1_24
-            golangci-lint
-            gotools
-            gopls
-            delve
-            goreleaser
-          ];
+  outputs =
+    { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs allSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      packages = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.buildGoModule {
+            pname = "herald";
+            version = "0.1.0";
+            subPackages = [ "." ];
+            src = self;
+            vendorHash = "sha256-IE7JMJ4DehwYxrkh5YCgw7yWdybxtCCxWQO7M/u6bno=";
+          };
+        }
+      );
 
-          shellHook = ''
-            echo "Herald development environment"
-            echo "Go version: $(go version)"
-            echo "golangci-lint version: $(golangci-lint version)"
-          '';
-        };
-      }
-    );
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              go_1_24
+              gopls
+              gotools
+              go-tools
+              golangci-lint
+              delve
+              goreleaser
+            ];
+
+            shellHook = ''
+              echo "Herald development environment"
+              echo "Go version: $(go version)"
+              echo "golangci-lint version: $(golangci-lint version)"
+            '';
+          };
+        }
+      );
+
+      apps = forAllSystems (
+        { pkgs }:
+        {
+          default = {
+            type = "app";
+            program = "${self.packages.${pkgs.system}.default}/bin/herald";
+          };
+        }
+      );
+    };
 }
